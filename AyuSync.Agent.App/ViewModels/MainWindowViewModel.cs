e05c1c018efb20c810710ada82404730a7272cd4
mr05je <ko190705la@gmail.com>
2023-07-08 04:48:30 +0300

    Init project, made base UI

---
+++ AyuSync.Agent.App/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,15 @@
+﻿using ReactiveUI;
+
+namespace AyuSync.Agent.App.ViewModels;
+
+public class MainWindowViewModel : ViewModelBase
+{
+    public bool EnableDebugMode 
+    { 
+        get => _enableDebugMode;
+        set { _enableDebugMode = value; this.RaisePropertyChanged(); }
+    }
+    private bool _enableDebugMode { get; set; }
+    
+    
+}
---
+++ AyuSync.Agent.App/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace AyuSync.Agent.App.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
